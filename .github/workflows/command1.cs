86499ede12473840e0f46fca0b2899da9f6273ff
jsakamoto <junichi.jp@gmail.com>
2025-12-27 23:22:09 +0900

    Initial commit

---
+++ .github/workflows/command1.cs
@@ -0,0 +1,6 @@
+// index.html をテキストとして読み取り...
+var indexHtml = File.ReadAllText("../../public/index.html");
+// <base href="/"> を <base href="/my-repo/" /> に置換して..
+indexHtml = indexHtml.Replace("<base href=\"/\" />", "<base href=\"/my-repo/\" />");
+// index.html に書き戻し
+File.WriteAllText("../../public/index.html", indexHtml);
